56b35e43ecdaea7aafaa0108924c5f4626410770
A997SI <jul.baroni@orange.fr>
2022-12-06 18:17:54 +0100

    fix : changement du nom du projet

--- Sprint_1/medicreen_P9_JB/src/main/java/com/softwareacademy/sprint1/p9jb/exceptions/DoesNotExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.softwareacademy.sprint1.p9jb.exceptions;
-
-public class DoesNotExistsException extends Exception{
-
-    public DoesNotExistsException(String s) {
-        super(s);
-    }
-}
